7696e1e0420b0a505439bbc7d19e3cfa17fd4209
nck.974 <nck.974@gmail.com>
2024-02-27 08:57:47 +0100

    Create unit test for the endpoints

--- src/main/java/com/nichoko/domain/mapper/FlightMapper.java
+++ src/main/java/com/nichoko/domain/mapper/FlightMapper.java
@@ -1,13 +1,14 @@
 package com.nichoko.domain.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import com.nichoko.domain.dao.Flight;
 import com.nichoko.domain.dto.FlightDTO;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
-@Mapper(componentModel = "cdi")
+@Mapper(componentModel = "cdi", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 @ApplicationScoped
 public interface FlightMapper {
 
