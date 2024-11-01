bd4a53633391ec3959c007c40e5b0d2e85b15d66
nck nck <nck.974@gmail.com>
2024-11-01 13:38:49 +0100

    Update proejct versions

--- build-image.ps1
+++ build-image.ps1
@@ -16,31 +16,32 @@ if (-not ($version) ) {
 # Print the command provided
 if ($project -eq "--angular") {
     Write-Host "Command: --angular"
-    # docker build -f flight-combination-finder-ng/docker/Dockerfile -t nck974/flight-combination-finder-ng:$version .
-    # cd flight-combination-finder-ng
-    # docker build -f docker/Dockerfile -t nck974/flight-combination-finder-ng:1.0.0 .
-    # docker tag nck974/flight-combination-finder-ng:1.0.0 nck974/flight-combination-finder-ng:latest
-    # docker push nck974/flight-combination-finder-ng:1.0.0
+    cd flight-combination-finder-ng
+    docker build -f Dockerfile -t nck974/flight-combination-finder-ng:$version .
+    docker tag nck974/flight-combination-finder-ng:$version nck974/flight-combination-finder-ng:latest
+    docker push nck974/flight-combination-finder-ng:$version
+    cd ..
+    $ImageName = "nck974/flight-combination-finder-ng"
 
 }
 if ($project -eq "--quarkus") {
     Write-Host "Building quarkus image..."
     # quarkus build
-    # docker build -f src/main/docker/Dockerfile.jvm -t nck974/flight-combination-finder:$version .
-    # docker build -f src/main/docker/Dockerfile.jvm -t nck974/flight-combination-finder:1.0.0 .
-    # docker tag nck974/flight-combination-finder:1.0.0 nck974/flight-combination-finder:latest
-    # docker push nck974/flight-combination-finder:1.0.0
-    # docker push nck974/flight-combination-finder:latest
+    docker build -f src/main/docker/Dockerfile.jvm -t nck974/flight-combination-finder:$version .
+    docker tag nck974/flight-combination-finder:$version nck974/flight-combination-finder:latest
+    docker push nck974/flight-combination-finder:$version
+    docker push nck974/flight-combination-finder:latest
     $ImageName = "nck974/flight-combination-finder"
-
-    # Run the docker images command and filter the output
-    $imageExists = docker images $ImageName | Select-String -Pattern "^$ImageName\s+$Tag\s+"
-
-    # Check if the image exists
-    if ($imageExists) {
-        Write-Host "The image $ImageName with tag $version exists."
-    } else {
-        Write-Host "The image $ImageName with tag $Tag does not exist."
 }
+
+# Check if the image exists
+# Run the docker images command and filter the output
+$imageExists = docker images $ImageName | Select-String -Pattern "^$ImageName\s+$Tag\s+"
+
+if ($imageExists) {
+    Write-Host "The image $ImageName with tag $version exists."
+} else {
+    Write-Host "The image $ImageName with tag $Tag does not exist."
 }
+
 
